3d2baa52037674ced92d4dd192cf8e456819677c
Mick Arias <mick756@gmail.com>
2019-07-25 10:03:31 -0700

    Added remove button and added stability

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -5,8 +5,9 @@ $(document).ready(function () {
 
     var $buttons = $("<div>").addClass("buttonNav");
     var $gifs = $("<div>").addClass("gifContainer");
-    var defaultQuickSearches = ["Minecraft", "Call of Duty", "Nazi Zombies", "Computers", "Laptops", "XBox", "PS3", "PS4", "GTAV", "Left 4 Dead", "Fortnite", "Apex Legends"];
+    var quickSearches = localStorage.getItem("searches") !== null ? localStorage.getItem("searches").split(",") : ["minecraft", "call of duty", "zombies", "computers", "laptops", "xbox", "ps3", "ps4", "gtav", "left 4 dead", "fortnite", "apex legends"];
     buildPage();
+
 
     /**
      * END VARIABLES
@@ -14,7 +15,7 @@ $(document).ready(function () {
      * BEGIN EVENTS
      */
 
-    $(document).on("click", ".gif", function() {
+    $(document).on("click", ".gif", function () {
         var $img = $(this);
         var currentVal = $img.attr("src");
         if (currentVal.includes("_s")) {
@@ -24,19 +25,38 @@ $(document).ready(function () {
         }
     });
 
-    $(document).on("click", ".searchButton", function() {
+    $(document).on("click", ".searchButton", function () {
         var $input = $(".searchBar");
         if ($input.val().length) {
             generateGifsFromSearch($input.val(), 20);
-            if (!defaultQuickSearches.includes($input.val())) {
-                defaultQuickSearches.push($input.val());
+            if (!quickSearches.includes($input.val())) {
+                quickSearches.push($input.val().toLowerCase());
                 addGIFButton($input.val());
             }
             $input.val("");
         }
+        localStorage.setItem("searches", quickSearches);
     });
 
-    $(document).on("click", ".gifSearchButton", function() {
+    $(document).on("click", ".removeButton", function () {
+        var $input = $(".searchBar");
+        if ($input.val().length && quickSearches.includes($input.val().toLowerCase())) {
+            console.log("CLICK");
+            $buttons.empty();
+            console.log(quickSearches);
+            quickSearchs = quickSearches.splice(quickSearches.indexOf($input.val().toLowerCase()), 1);
+            console.log(quickSearches);
+            localStorage.setItem("searches", quickSearches);
+            quickSearches = localStorage.getItem("searches").split(",");
+            quickSearches.forEach(search => {
+                //if (search !== empty) {
+                    addGIFButton(search);
+            });
+            $input.val("");
+        }
+    });
+
+    $(document).on("click", ".gifSearchButton", function () {
         generateGifsFromSearch($(this).text(), 20);
     });
 
@@ -52,8 +72,9 @@ $(document).ready(function () {
         var $searchContainer = $("<div>").addClass("searchBarContainer");
         var $searchBar = $("<input>").addClass("searchBar");
         var $searchButton = $("<button>").addClass("searchButton").text("Search");
-        $(".container").append($searchContainer.append($searchBar, $searchButton), $gifs);
-        defaultQuickSearches.forEach(search => {
+        var $removeButton = $("<button>").addClass("removeButton").text("Remove");
+        $(".container").append($searchContainer.append($searchBar, $searchButton, $removeButton), $gifs);
+        quickSearches.forEach(search => {
             addGIFButton(search);
         });
     }
@@ -80,8 +101,8 @@ $(document).ready(function () {
             }).then(response => {
                 var $newGifElement = $("<div>").addClass("gifElement");
                 var title = response.data[i].title.toUpperCase().replace("GIF", "").split(" ");
-                var $gifTitle = $("<div>").addClass("gifTitle").text(title[0]);
-                var $gifRating = $("<div>").addClass("gifRating").text("Rating: " + response.data[i].rating);
+                var $gifTitle = $("<div>").addClass("gifTitle").text("Title: " + title[0]);
+                var $gifRating = $("<div>").addClass("gifRating").text("Rating: " + response.data[i].rating.toUpperCase());
                 var $gif = $("<img>").addClass("gif").attr("src", response.data[i].images.original_still.url).attr("movingPicture", response.data[i].images.original.url).attr("stillPicture", response.data[i].images.original_still.url);
                 $newGifElement.append($gifTitle, $gifRating, $gif);
                 $gifs.append($newGifElement);
